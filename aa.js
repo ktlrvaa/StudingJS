c040dcd4d302d2c0646c35e721312113caa9438e
ktlrvaa <ktlrv@bk.ru>
2023-11-03 18:53:00 +0300

    Изучение JS на практике

---
+++ aa.js
@@ -0,0 +1,8 @@
+const obj = { a: 5, b: '6', c: [] }
+
+obj.a = 7.7
+
+console.log(Object.values(obj).map(x => typeof x))
+    
+    
+    
--- apply-javascript.js
+++ apply-javascript.js
@@ -6,6 +6,17 @@ function createParagraph() {
     document.body.appendChild(para);
 }
 
+function showList(){
+    var ulElement = document.createElement("ul");
+    for(var i = 0; i<10; i++){
+        var liElement = document.createElement("li");
+        liElement.className = "li-class";
+        liElement.textContent = i + " элемент";
+        ulElement.appendChild(liElement);
+    }
+    document.body.appendChild(ulElement);
+}
+
 /*
   1. Получаем ссылки на все кнопки на странице в виде массива.
   2. Перебераем все кнопки и добавляем к ним отслеживатель события нажатия.
@@ -13,8 +24,14 @@ function createParagraph() {
   При нажатии любой кнопки, будет выполняться функция createParagraph().
 */
 
-var buttons = document.querySelectorAll("button");
+// var buttons = document.querySelectorAll("button");
 
-for (var i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", createParagraph);
-}
+// for (var i = 0; i < buttons.length; i++) {
+//     buttons[i].addEventListener("click", createParagraph);
+// }
+
+var btn1 = document.getElementById("ClickMeBtn");
+btn1.addEventListener("click", createParagraph);
+var btn2 = document.getElementById("ShowListBtn");
+btn2.addEventListener("click", showList);
+
